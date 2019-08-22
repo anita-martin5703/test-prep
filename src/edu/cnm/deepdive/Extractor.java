6763a68ca305c6ac9b54e22fce1c10948ac3521a
Anita <amartin98@cnm.edu.com>
2019-08-22 10:20:54 -0600

    Comparing.

--- src/edu/cnm/deepdive/Extractor.java
+++ src/edu/cnm/deepdive/Extractor.java
@@ -9,7 +9,7 @@ public class Extractor {
   public static void main(String[] args) {
     String source = (args.length > 0) ? args[0] : "Hello, World!";
     Set<Character> chars = new TreeSet<>((c1, c2) ->
-        Character.compare(Character.toUpperCase(c1), Character.toUpperCase(c2)));
+        Comparator.comparingInt((Character c) -> Character.toUpperCase(c)).compare(c1, c2));
     for (char c : source.toCharArray()) {
       chars.add(c);
     }
